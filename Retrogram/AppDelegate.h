c16e57ddbe662a0b66bfcdb2ec310ef459097043
Kyle Yoon <apps.yoon@gmail.com>
2014-10-27 14:46:47 -0500

    Initial Commit

---
+++ Retrogram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Retrogram
+//
+//  Created by Kyle Yoon on 10/27/14.
+//  Copyright (c) 2014 yoonapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
